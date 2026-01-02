22ade3b3b294faf0eeda1c59d2aa1f6e3188a630
Rahulsannamath <rahul.sannamath@gmail.com>
2026-01-02 22:53:58 +0530

    changed UI theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
 
+document.documentElement.setAttribute("data-bs-theme", "dark");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
